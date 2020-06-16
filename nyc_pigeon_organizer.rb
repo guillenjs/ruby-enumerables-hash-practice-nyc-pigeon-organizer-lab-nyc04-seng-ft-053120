6b1801e65efa35b0d4bb10955d8288b4e243d09f
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-16 18:48:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,13 +12,14 @@ def nyc_pigeon_organizer(data)
 data.each_with_object({}) do |(k, v), result| #goes through each hash and value to return a hash
   v.each do |inner_key, inner_value| #this block will go in the inner key, inner value thats holds the names
       inner_value.each do |names|# this block will go through each name that we have
-        if !result[names]
-          result[names] = {}
+binding.pry
+        if !result[names]# if the combination of result[names] doesnt exist
+          result[names] = {} #then result name goes into hash
         end
-          if !result[names][k]
-            result[names][k] = []
+          if !result[names][k] #if the combination of !result[names][k] doesnt exist
+            result[names][k] = []#then combination goes into array
           end
-          result[names][k] << inner_key.to_s
+          result[names][k] << inner_key.to_s #return new key data combination and push the inner key as values
 
       end
     end
